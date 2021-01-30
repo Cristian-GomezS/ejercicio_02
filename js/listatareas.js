0da022a9d91e4c4cd588fb322691d58e18fe728b
Cristian Gomez <crisstianc@gmail.com>
2021-01-30 23:30:03 +0100

    He modificado el codigo y añadido las hojas archivos para el javascript

---
+++ js/listatareas.js
@@ -0,0 +1,25 @@
+let tareas = new Array(
+    {
+        id: 1,
+        tarea: 'comprar',
+        prioridad: 2,
+    },
+
+    {
+        id: 2,
+        tarea: 'estudiar',
+        prioridad: 3,
+    },
+
+    {
+        id: 3,
+        tarea: 'salir',
+        prioridad: 1,
+    },
+
+    {
+        id: 4,
+        tarea: 'pagar el alquiler',
+        prioridad: 3,
+    },
+)
